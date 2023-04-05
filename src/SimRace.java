b38b2246ac7ed818da079550eb0854f7c7bd74ee
Daniel <danielfmhaw@gamil.com>
2023-04-05 10:25:00 +0200

    Besprochen

--- src/SimRace.java
+++ src/SimRace.java
@@ -4,33 +4,34 @@ import java.util.Collections;
 import java.util.List;
 
 public class SimRace {
-    int anzahl_Autos;
+    int  anzahl_Autos;
     int rundenanzahl;
 
-    public SimRace(int rundenanzahl){
+    public SimRace(int rundenanzahl,int anzahl_Autos){
         this.rundenanzahl=rundenanzahl;
+        this.anzahl_Autos=anzahl_Autos;
     }
 
     public static void main(String[] args) {
-        Car auto1= new Car("Wagen 0");
-        Car auto2= new Car("Wagen 1");
-        Car auto3= new Car("Wagen 2");
-        Car auto4= new Car("Wagen 3");
-        Car auto5= new Car("Wagen 4");
-        ArrayList <Car>autos= new ArrayList<>( Arrays.asList(auto1,auto2,auto3,auto4,auto5));
-        SimRace test= new SimRace(11);
-        //SimRace test= new SimRace(10_000_000);
+        SimRace test= new SimRace(10_000_000,10);
+        //SimRace test= new SimRace(10,7);
+        List <Car>autos= test.autosErstellen();
         test.autosAufBahn(autos);
     }
 
-    /**
-        Aufgabe 1- Methode
-     */
-    public void autosAufBahn(List<Car> autos){
-        //Thread starten
-        for(int i=0;i<autos.size();i++){
-            autos.get(i).start();
+    public List<Car> autosErstellen(){
+        List<Car> autos= new ArrayList<>();
+        for (int i=0;i<anzahl_Autos;i++){
+            String name= "Wagen " +i;
+            Car car= new Car(name);
+            car.start();
+            autos.add(car);
         }
+        return autos;
+    }
+
+    public void autosAufBahn(List<Car> autos){
+
         long startTime = System.nanoTime();
 
         //Anzahl an Runden entsprechend oft neue Rundenzeit berechnen und warten
@@ -47,7 +48,7 @@ public class SimRace {
             //System.out.println("Runde "+j+" beendet");
         }
         
-        //Thread warten
+        //Thread warten (alle im Ziel)
         try {
             for(int i=0;i<autos.size();i++){
                 autos.get(i).join();
@@ -62,11 +63,14 @@ public class SimRace {
     }
 
     private void konsolenAusgabe(List<Car> autos){
+        int gesamtzeit=0;
         Collections.sort(autos);
         System.out.println("");
         System.out.println("****Endstand****");
         for(int i=0;i<autos.size();i++){
+            gesamtzeit+=autos.get(i).gesamtzeit;
             System.out.println((i+1)+".Platz: "+autos.get(i).toString()+" Zeit:"+autos.get(i).gesamtzeit);
         }
+        System.out.println(gesamtzeit);
     }
 }
